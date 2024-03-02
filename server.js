c17364bad6a83701c45b0754fab5c5037afa239e
GermanSTr <gershev1994@gmail.com>
2024-03-02 15:39:04 +0200

    env

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const formatLogger = app.get("env") === "dev" ? "dev" : "short";
 
-app.use(logger("dev"));
+app.use(logger(formatLogger));
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json("myJson");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running_1");
 });

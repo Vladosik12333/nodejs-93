f026c69b4db02c8aa5c7fb01f88c3f554b6a67ed
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-03-02 16:25:24 +0200

    add error middlewares

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const error = require("./src/helpers/error.js");
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -9,8 +11,29 @@ const formatLogger = app.get("env") === "dev" ? "dev" : "short";
 app.use(logger(formatLogger));
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
+  const myerror = error(401, "my error 1");
+
+  next(myerror);
+  return;
   res.json("myJson");
+});
+
+app.get("/car", (req, res, next) => {
+  const myerror = error(402, "my error 2");
+
+  next(myerror);
+  return;
+  res.json("my car");
+});
+
+app.use((_, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
+app.use((error, _, res, __) => {
+  const { status = 500, message = "Server internal error" } = error;
+  res.status(status).json({ message, status });
 });
 
 app.listen(process.env.PORT, () => {
